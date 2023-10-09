2441c6fb08d6fdf75d8d502c5c5414512e237ea2
venkateshgangoji <venkateshgangoji@gmail.com>
2023-10-10 01:53:44 +0530

    intro

--- src/components/introduction/introduction.jsx
+++ src/components/introduction/introduction.jsx
@@ -6,7 +6,7 @@ const Intro=()=>{
         <header>
             <div className="container header__container">
                 <h5>Hello I'm</h5>
-                <h1>Venkatesh Gangoji</h1>
+                <h1 id="user-detail-name">Venkatesh Gangoji</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
                 <CTA/>
 
